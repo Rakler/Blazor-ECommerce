8e71d0bd869985d7de333008fe7ace84768a8d98
Rakler <35872691+Rakler@users.noreply.github.com>
2022-06-14 19:29:21 +0100

    Created a test Product API controller.

---
+++ ECommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<Product> GetProduct()
+        {
+            IEnumerable<Product> products = new List<Product> {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Hello world",
+                    Description = "A product"
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Hello world 2",
+                    Description = "Second Hello world"
+                }
+        };    
+
+
+            return Ok(products);
+        }
+
+    }
+}
--- ECommerce/Shared/Product.cs
+++ ECommerce/Shared/Product.cs
@@ -8,5 +8,8 @@ namespace ECommerce.Shared
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
