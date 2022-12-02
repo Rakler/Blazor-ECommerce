5cacf78d6284c0d9b02f95f08a600ac516d9c288
Rakler <35872691+Rakler@users.noreply.github.com>
2022-12-02 15:15:22 +0000

    AddedProductVariants

--- ECommerce/Client/Shared/BaseClasses/ProductListBase.cs
+++ ECommerce/Client/Shared/BaseClasses/ProductListBase.cs
@@ -22,5 +22,22 @@ namespace ECommerce.Client.Shared.BaseClasses
         {
             ProductService.ProductsChanged -= StateHasChanged;
         }
+
+        public string GetPriceText(Product product)
+        {
+            var variants = product.Variants;
+
+            if(variants.Count == 0)
+            {
+                return string.Empty;
+            }
+            else if(variants.Count == 1)
+            {
+                return $"${variants[0].Price}";
+            }
+            decimal minPrice = variants.Min(v => v.Price);
+
+            return $"Starting at ${minPrice}";
+        }
     }
 }
